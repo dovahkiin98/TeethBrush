3c796064b8f32d10c76f484c6e8854c338a6424e
Ahmad-disruptem <ahmad.s2@disruptem.com>
2025-09-12 16:40:08 +0300

    Update `SettingsDataStore.setDuration()` to always use `Dispatchers.IO`

--- app/src/main/java/net/inferno/teethbrush/ui/main/SettingsDataStore.kt
+++ app/src/main/java/net/inferno/teethbrush/ui/main/SettingsDataStore.kt
@@ -1,14 +1,19 @@
 package net.inferno.teethbrush.ui.main
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -24,8 +29,10 @@ class SettingsDataStore @Inject constructor(
         it[DURATION_KEY] ?: (2 * 60)
     }
 
-    suspend fun setDuration(duration: Int) = dataStore.edit {
-        it[DURATION_KEY] = duration
+    suspend fun setDuration(duration: Int) = withContext(Dispatchers.IO) {
+        dataStore.edit {
+            it[DURATION_KEY] = duration
+        }
     }
 
     companion object {
