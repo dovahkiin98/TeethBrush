129f5ded9aa2b260e50a053e37d103bb69be0dce
Ahmad-disruptem <ahmad.s2@disruptem.com>
2026-08-12 15:22:44 +0300

    Remove unused imports in `SettingsDataStore`

--- app/src/main/java/net/inferno/teethbrush/ui/main/SettingsDataStore.kt
+++ app/src/main/java/net/inferno/teethbrush/ui/main/SettingsDataStore.kt
@@ -1,7 +1,6 @@
 package net.inferno.teethbrush.ui.main
 
 import android.content.Context
-import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
@@ -10,9 +9,7 @@ import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
